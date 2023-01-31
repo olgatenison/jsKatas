1dfc4d3997d0c7562274cfe74dfef32d150d5cb6
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-01-31 15:24:18 +0200

    ad js

--- js/01numbers.js
+++ js/01numbers.js
@@ -92,3 +92,93 @@ result += '1';
 }
 return result; 
 */
+/* 9 Determine the area of the largest square that can fit inside a circle with radius r.*/
+
+function areaLargestSquare(r) {
+  const d = r + r;
+  return Math.pow(d, 2) / 2;
+  // return ((r + r) * (r + r)) / 2;
+}
+
+/*Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.*/
+
+function digits(n) {
+  return n.toString().length;
+}
+
+/*Very simple, given an integer or a floating-point number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34*/
+
+function opposite(number) {
+  return number * -1;
+}
+/*You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+Function should return true if it is possible and false if not*/
+/*const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if (fuelLeft * mpg >= distanceToPump) {
+    return true;
+  } else {
+    return false;
+  }
+};
+*/
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  result = fuelLeft * mpg >= distanceToPump ? true : false;
+  return result;
+};
+
+/*You are given two interior angles (in degrees) of a triangle.
+Write a function to return the 3rd.
+Note: only positive integers will be tested.*/
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+
+/*Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+This function should return a number (final grade). There are four types of final grades:
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases*/
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.*/
+
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+}
+
+//  return n % 2 == 0 ? n * 8 : n * 9
+/*Perimeter sequence The first three stages of a sequence are shown.
+blocks The blocksize is a by a and a ≥ 1.
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?*/
+
+function perimeterSequence(a, n) {
+  let result = n * a * 2 + 2 * n * a;
+  return result;
+}
+//return 4 * n * a;
--- js/01string.js
+++ js/01string.js
@@ -55,3 +55,118 @@ const stringToNumber = function (str) {
 function toBinary(n) {
   return Number(n.toString(2));
 }
+
+//Return the type of the sum of the two arguments
+
+function typeOfSum(a, b) {
+  let sum = a + b;
+  let result = typeof sum;
+  return result;
+}
+/*After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d). */
+function rentalCarCost(d) {
+  let result;
+  let costPerDay = 40;
+  if (d >= 7) {
+    result = d * costPerDay - 50;
+  } else if (d >= 3 && d <= 7) {
+    result = d * costPerDay - 20;
+  } else {
+    result = d * costPerDay;
+  }
+  return result;
+}
+
+/*You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+For example, when the input is green, output should be yellow.*/
+
+function updateLight(current) {
+  switch (current) {
+    case "green":
+      return "yellow";
+      break;
+    case "yellow":
+      return "red";
+      break;
+    default:
+      return "green";
+  }
+}
+
+/*DESCRIPTION:
+When provided with a number between 0-9, return it in words.
+Input :: 1
+Output :: "One".
+If your language supports it, try using a switch statement.*/
+
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+      break;
+    case 1:
+      return "One";
+      break;
+    case 2:
+      return "Two";
+      break;
+    case 3:
+      return "Three";
+      break;
+    case 4:
+      return "Four";
+      break;
+    case 5:
+      return "Five";
+      break;
+    case 6:
+      return "Six";
+      break;
+    case 7:
+      return "Seven";
+      break;
+    case 8:
+      return "Eight";
+      break;
+    case 9:
+      return "Nine";
+      break;
+    default:
+      return "Not a valid number";
+      break;
+  }
+}
+
+/*Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false*/
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+/*It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.*/
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+//const removeChar = str => str.slice(1,-1)
+//return str.substring(1, str.length-1);
+
+/*Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"*/
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
