ecb99582308980e85a6817f65e211c1d1e0a1f5b
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-12 22:16:39 +0200

    update strings js

--- js/01string.js
+++ js/01string.js
@@ -170,3 +170,44 @@ Examples (input -> output)
 function repeatStr(n, s) {
   return s.repeat(n);
 }
+
+//coursera task 1
+
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != "number") {
+      throw new ReferenceError("the first argument is not a number");
+    } else if (typeof b != "number") {
+      throw new ReferenceError("the second argument is not a number");
+    } else {
+      console.log(a + b);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
+}
+addTwoNums(5, "5");
+console.log("It still works");
+
+//coursera task 2
+
+function letterFinder(word, match) {
+  var condition1 = typeof word == "string" && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+  var condition2 = typeof match == "string" && match.length == 1; //if the match is a string and the length is equal to 1
+  if (condition1 && condition2) {
+    //if both condition matches
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        //check if the character at this i position in the word is equal to the match
+        console.log("Found the", match, "at", i);
+      } else {
+        console.log("---No match found at", i);
+      }
+    }
+  } else {
+    //if the requirements don't match
+    console.log("Please pass correct arguments to the function");
+  }
+}
+letterFinder([], []);
+letterFinder("cat", "c");
