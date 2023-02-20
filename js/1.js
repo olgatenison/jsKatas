b1006c48fe6d8cf4d9191d8ab9f57e1a432a5d40
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-21 00:06:39 +0200

    new tasks js arrays

--- js/1.js
+++ js/1.js
@@ -107,11 +107,72 @@ const fruitsArrayLength = fruits.length;
 /*Задание 8
 Объяви две переменные:
 Имя переменной	  Ожидаемое значение
-lastElementIndex	Индекс последнего элемента масcива fruits через длина_массива - 1
-lastElement	Значение последнего элемента массива*/
+lastElementIndex	Индекс последнего элемента масcива 
+                  fruits через длина_массива - 1
+lastElement	      Значение последнего элемента массива
 
 const lastElementIndex = fruits.length - 1;
-const lastElement = fruits[lastElementIndex];
+const lastElement = fruits[lastElementIndex];*/
+
+/*Задание 9
+Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array.
+
+function getExtremeElements(array) {
+  return [array[0], array[array.length - 1]];
+}*/
+
+/*Задание 10
+Дополни код функции splitMessage(message, delimiter) так, чтобы она возвращала в переменной words результат разделения строки message по разделителю delimiter - массив строк.
+
+function splitMessage(message, delimiter) {
+  let words;
+  words = message.split(delimiter);
+  return words;
+}*/
+
+/*Задание 11
+Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
+Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
+Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.*/
+
+function calculateEngravingPrice(message, pricePerWord) {
+  let words = message.split(" ");
+  let wordCount = words.length;
+  let totalPrice = wordCount * pricePerWord;
+  return totalPrice;
+}
+
+/*Задание 12
+Дополни код функции makeStringFromArray(array, delimiter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimiter - строку.*/
+
+function makeStringFromArray(array, delimiter) {
+  let string;
+  string = array.join(delimiter);
+  return string;
+}
+
+/*Задание 13
+Термин slug - это человеко-понятный уникальный идентификатор, который используется в веб-разработке для создания читабельных URL-адресов.
+Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx, можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+
+Внимание
+Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки.
+Значением параметра title будут строки, слова которых разделены только пробелами
+Все символы slug должны быть в нижнем регистре
+Все слова slug должна быть разделены тире*/
+
+function slugify(title) {
+  let lowerTitle = title.toLowerCase();
+  let lowerWords = lowerTitle.split(" ");
+  let slug = lowerWords.join("-");
+
+  return slug;
+  // return title.toLowerCase().split(" ").join("-");
+}
+
+slugify("How to become a JUNIOR developer in TWO WEEKS");
 
 /*Задание 32
 Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
