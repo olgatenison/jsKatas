5dd59887d2db85177340874b0927f5c8a11cf1b0
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-02-26 23:30:52 +0200

    Update 01string.js

--- js/01string.js
+++ js/01string.js
@@ -211,3 +211,20 @@ function letterFinder(word, match) {
 }
 letterFinder([], []);
 letterFinder("cat", "c");
+
+/*Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: for this kata y isn't considered a vowel.*/
+
+function disemvowel(str) {
+  const vowels = "aeiouAEIOU";
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!vowels.includes(str[i])) {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
