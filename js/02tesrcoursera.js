11b7612c72c65cf7e2ad0bef2c0d84e243063692
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-03-10 16:24:12 +0200

    add clasess coursera

---
+++ js/02tesrcoursera.js
@@ -0,0 +1,61 @@
+// class Train {
+//   constructor(color, lightsOn) {
+//     this.color = color;
+//     this.lightsOn = lightsOn;
+//   }
+// }
+
+class Train {
+  constructor(color, lightsOn) {
+    this.color = color;
+    this.lightsOn = lightsOn;
+  }
+  toggleLights() {
+    this.lightsOn = !this.lightsOn;
+  }
+  lightsStatus() {
+    console.log("Lights on?", this.lightsOn);
+  }
+  getSelf() {
+    console.log(this);
+  }
+  getPrototype() {
+    var proto = Object.getPrototypeOf(this);
+    console.log(proto);
+  }
+}
+
+new Train();
+var myFirstTrain = new Train("red", false);
+
+console.log(myFirstTrain); // Train {color: 'red', lightsOn: false}
+
+var mySecondTrain = new Train("blue", false);
+var myThirdTrain = new Train("blue", false);
+
+console.log(mySecondTrain);
+console.log(myThirdTrain);
+
+var train4 = new Train("red", false);
+
+train4.toggleLights(); // undefined
+train4.lightsStatus(); // Lights on? true
+train4.getSelf(); // Train {color: 'red', lightsOn: true}
+train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
+
+class HighSpeedTrain extends Train {
+  constructor(passengers, highSpeedOn, color, lightsOn) {
+    super(color, lightsOn);
+    this.passengers = passengers;
+    this.highSpeedOn = highSpeedOn;
+  }
+  toggleHighSpeed() {
+    this.highSpeedOn = !this.highSpeedOn;
+    console.log("High speed status:", this.highSpeedOn);
+  }
+  toggleLights() {
+    super.toggleLigths();
+    super.lightsStatus();
+    console.log("Lights are 100% operational.");
+  }
+}
