57be12819f1780acc67e3a4b156eca067dac99ea
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-08-19 13:05:55 +0300

    add fibonachi

--- js/draco.js
+++ js/draco.js
@@ -6,23 +6,3 @@ function hero(bullets, dragons) {
   // возвращаем true, в противном случае возвращаем false
   return bullets >= dragons * 2;
 }
-
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location;
-
-apartment.location.country = "Jamaica";
-apartment.location.city = "Kingston";
-console.log(apartment);
--- js/fibonachi.js
+++ js/fibonachi.js
@@ -6,8 +6,40 @@
 // Because 2 is the 4th number in the Fibonacci Sequence.
 // For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
 
-function fibo(n) {
-  let arr = [];
-  for (let i of arr) {
+function nthFibo(n) {
+  let first = 0;
+  let second = 1;
+  let fibonacciSequence = [first, second];
+
+  if (n <= 0) {
+    return "Invalid input";
+  } else if (n === 1) {
+    return first;
+  } else if (n === 2) {
+    return second;
+  } else if (n > 2) {
+    for (let i = 2; i < n; i++) {
+      // вираховуємо значення наступного як суму двох попередніх
+      let nextNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+      // додаємо в масив
+      fibonacciSequence.push(nextNumber);
+    }
   }
+  let last = fibonacciSequence[fibonacciSequence.length - 1];
+  return last;
 }
+
+console.log(nthFibo(14));
+console.log(nthFibo(4));
+
+// 1
+// function nthFibo(n) {
+//   return n < 2 ? 0 : n == 2 ? 1 : nthFibo(n-1) + nthFibo(n-2);
+// }
+
+// 2
+// function nthFibo(n) {
+//   let [prev, curr] = [0, 1];
+//   for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr];
+//   return prev;
+// }
