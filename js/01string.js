0d6424f359246014fef7fee86e08fcfb41173e93
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-11 21:07:23 +0300

    arr

--- js/01string.js
+++ js/01string.js
@@ -228,3 +228,27 @@ function disemvowel(str) {
   }
   return newStr;
 }
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers) {
+  let arr = [];
+  arr.push(numbers.split(" "));
+  console.log(arr);
+}
+
+a.sort((x, y) => {
+  x = x.split(".");
+  y = y.split(".");
+  return x[0] - y[0] || x[1] - y[1];
+});
+
+console.log(a.join(" "));
